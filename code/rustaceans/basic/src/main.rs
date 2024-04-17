396b94c1b011caf217ab4d46a7f654c529313782
jiasenwqbr <jasonjiasen@gmail.com>
2024-04-17 11:51:28 +0800

    Rc<T>, the Reference Counted Smart Pointer

--- code/rustaceans/basic/src/main.rs
+++ code/rustaceans/basic/src/main.rs
@@ -1,28 +1,4 @@
-use std::ops::Deref;
-
 fn main() {
-    let x = 5;
-    let y = Box::new(5);
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-
-    let a = 5;
-    let b = MyBox::new(5);
-    assert_eq!(5, a);
-    assert_eq!(5, *(b.deref()));
-    assert_eq!(5, *b);
-}
-
-struct MyBox<T>(T);
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
-}
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
+    // let x = 5;
+    // let y = &mut x; //error: could not compile `borrowing` due to previous error
 }
