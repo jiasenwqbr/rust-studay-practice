9a22182956484c33ef305084b9433f2d4bb0bece
jiasenwqbr <jasonjiasen@gmail.com>
2024-04-18 14:53:15 +0800

    Advanced Traits

--- code/rustaceans/basic/src/main.rs
+++ code/rustaceans/basic/src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use core::fmt;
+
+struct Wrapper(Vec<String>);
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+fn main() {
+    let w = Wrapper(vec![String::from("hello"), String::from(" world")]);
+    println!("w = {}", w);
+}
